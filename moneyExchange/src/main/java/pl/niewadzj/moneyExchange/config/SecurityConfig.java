b4833eb5f55bb88c091c9b5a08905b488da9aa5c
JakubNiewadzi <01169600@pw.edu.pl>
2024-02-27 19:32:55 +0100

    simple security

---
+++ moneyExchange/src/main/java/pl/niewadzj/moneyExchange/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package pl.niewadzj.moneyExchange.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public final class SecurityConfig {
+
+    private final CorsConfigurationSource corsConfigurationSource;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("*").permitAll());
+
+        return http.build();
+    }
+}
